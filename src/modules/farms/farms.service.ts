52eb86633981790a194785f975793bf9e5c1d4d2
Manu Grandio <manugrandio@fastmail.fm>
2023-02-27 23:13:57 +0100

    store fetched coordinates in database WIP

--- src/modules/farms/farms.service.ts
+++ src/modules/farms/farms.service.ts
@@ -1,7 +1,8 @@
 import { DeepPartial, Repository } from "typeorm";
+import dataSource from "orm/orm.config";
 import { Farm } from "./entities/farm.entity";
 import { CreateFarmDto } from "./dto/create-farm.dto";
-import dataSource from "orm/orm.config";
+import { Geocoding } from "services/geocoding.service";
 
 export class FarmsService {
   private readonly farmsRepository: Repository<Farm>;
@@ -11,7 +12,17 @@ export class FarmsService {
   }
 
   public async createFarm(data: CreateFarmDto): Promise<Farm> {
-    const farmData: DeepPartial<Farm> = data;
+    const { address } = data;
+    let coordinatesStr;
+    if (address !== undefined) {
+      const geocoding = new Geocoding();
+      const coordinates = await geocoding.getCoordinates(address);
+      if (coordinates !== null) {
+        const { latitude, longitude } = coordinates;
+        coordinatesStr = `(${latitude}, ${longitude})`;
+      }
+    }
+    const farmData: DeepPartial<Farm> = { ...data, coordinates: coordinatesStr };
     const newFarm = this.farmsRepository.create(farmData);
     return this.farmsRepository.save(newFarm);
   }
